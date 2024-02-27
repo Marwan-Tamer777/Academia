35269ad8e8c8ec9ba5390f7dfaf2b07ef0e4c71f
mohamed-3090 <mr.mmm2002@gmail.com>
2024-02-27 15:40:04 +0200

    setting-the-project-and-login-page

---
+++ Web App/src/app/page.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <a style={{top:'50%', right:'50%', color:'white', textDecoration:'none'}} href="/login">login</a>
+    </div>
+  )
+}
