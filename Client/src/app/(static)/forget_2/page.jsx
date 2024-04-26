799702c3a6494134de243a25ccdc84c2dc03d4b1
mohamed-3090 <mr.mmm2002@gmail.com>
2024-04-26 23:50:12 +0300

    update otp screen
    rename web app to client, remove academia folder from server directory

--- Client/src/app/(static)/forget_2/page.jsx
+++ Client/src/app/(static)/forget_2/page.jsx
@@ -1,7 +1,7 @@
 import styles from "./page.module.css";
 import Form from "@/components/Form/Form";
 import Button from "@/components/Button/Button";
-import Input from "@/components/Input/Input";
+import OTP from "@/components/OTP/OTP";
 import FormHead from "@/components/FormHead/FormHead";
 
 export default function forget_2() {
@@ -12,14 +12,11 @@ export default function forget_2() {
           title="رمز التحقق - OTP"
           sub="أدخل الرمز الذي أرسلناه إلي عناو بريد التالي"
         />
-        <Input text="البريد الإلكتروني" type="username" />
+        <OTP />
         <Button text="تحقق من الرمز" />
-        <p style={{ fontSize: "14px", color: "#1D3149CC" }}>
-          ليس لديك حساب؟{" "}
-          <a href="/" style={{ color: "#ff7e31" }}>
-            تواصل مع الدعم
+          <a href="/" style={{ color: "#ff7e31", textDecoration:"none" }}>
+            إعادة إرسال الرمز - 35 ثانية
           </a>
-        </p>{" "}
       </Form>
     </div>
   );
