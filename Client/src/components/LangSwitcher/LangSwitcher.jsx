60c046f926179edf90d835102c73a27d9befa6ab
mohamed-3090 <mr.mmm2002@gmail.com>
2024-06-20 00:55:52 +0300

    add lang switcher

--- Client/src/components/LangSwitcher/LangSwitcher.jsx
+++ Client/src/components/LangSwitcher/LangSwitcher.jsx
@@ -1,58 +1,19 @@
-// 'use client'; // This directive ensures the component is treated as a Client Component
-
-// import React from 'react';
-// import { usePathname, useSearchParams, useRouter } from 'next/navigation';
-
-// const LanguageSwitcher = () => {
-//   const router = useRouter();
-//   const pathname = usePathname();
-//   const searchParams = useSearchParams();
-//   const locales = ['en', 'fr', 'es'];
-//   const locale = searchParams.get('locale') || 'en';
-
-//   const changeLanguage = (lng) => {
-//     const params = new URLSearchParams(searchParams);
-//     params.set('locale', lng);
-//     router.push(`${pathname}?${params.toString()}`);
-//   };
-
-//   return (
-//     <div>
-//       {locales.map((lng) => (
-//         <button key={lng} onClick={() => changeLanguage(lng)} disabled={lng === locale}>
-//           {lng}
-//         </button>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default LanguageSwitcher;
-
-
-// app/_components/LocaleSwitcher.tsx
-
 "use client";
 
-import {
-  localeNames,
-  locales,
-  usePathname,
-  useRouter,
-} from "@/i18n.config";
+import { localeNames, locales, usePathname, useRouter } from "@/navigation";
+import styles from "./LangSwitcher.module.css";
 
-export default function LocaleSwitcher({
-  locale,
-}) {
+import Image from "next/image";
+import {EG} from "/public/navbar/eg.jpg";
+
+export default function LocaleSwitcher({ locale }) {
   // `pathname` will contain the current
   // route without the locale e.g. `/about`...
   const pathname = usePathname();
   const router = useRouter();
 
-  const changeLocale = (
-    event,
-  ) => {
-    const newLocale = event.target.value ;
+  const changeLocale = (event) => {
+    const newLocale = event.target.value;
 
     // ...if the user chose Arabic ("ar-eg"),
     // router.replace() will prefix the pathname
@@ -62,15 +23,16 @@ export default function LocaleSwitcher({
   };
 
   return (
-    <div>
+    <div className={styles.langSwitcher}>
       <select
         value={locale}
         onChange={changeLocale}
-        className="..."
+        className={styles.langSwitcher}
       >
         {locales.map((loc) => (
-          <option key={loc} value={loc}>
+          <option key={loc} value={loc} className={styles.langs}>
             {localeNames[loc]}
+            {/* <Image src={EG}/> */}
           </option>
         ))}
       </select>
